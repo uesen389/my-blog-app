c8069f5874dc461e7acf83709d8cb3148537c640
HikaruMAC <uesen389@gmail.com>
2025-12-13 21:17:21 +0900

    Fix image paths and add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // GitHub Pagesがサブディレクトリ（リポジトリ名）で公開される場合は以下を有効化してください
-  // basePath: '/my-blog-app',
+  basePath: '/my-blog-app',
+  assetPrefix: '/my-blog-app',
 };
 
 export default nextConfig;
