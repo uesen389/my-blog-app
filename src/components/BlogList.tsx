cf2480295ae2f37263bc99be80731aee9a03c024
HikaruMAC <uesen389@gmail.com>
2025-12-25 11:45:33 +0900

    Fix duplicate function definition

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -58,12 +58,6 @@ function BlogListContent({ posts, settings }: BlogListProps) {
 
   const sortedArchives = Object.entries(archives).sort(([a], [b]) => b.localeCompare(a)) as [string, number][];
 
-  // Helper to format YYYY-MM to Japanese
-  const formatArchiveDate = (ym: string) => {
-    const [y, m] = ym.split('-');
-    return `${y}年${parseInt(m)}月`;
-  };
-
   // Filter posts for Main Content
   let filteredPosts = publishedPosts;
   
